58ec04077ebe91fb5f9f250b727101d028e543bb
Behzad <depotentia@gmail.com>
2026-01-28 12:46:47 +0430

    inventory table added

--- src/features/router/Gateway.jsx
+++ src/features/router/Gateway.jsx
@@ -1,11 +1,13 @@
 import { Route, Routes } from "react-router";
 import Dashboard from "../dashboard/pages/Dashboard";
+import InventoryPage from "../inventory/pages/Inventory";
 
 function Gateway() {
   return (
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/inventory" element={<InventoryPage />} />
     </Routes>
   );
 }
